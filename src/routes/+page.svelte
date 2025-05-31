a4b6875fecfd16c35225f00d1f9909ea00105d52
ParrotOS <mukto2004@gmail.com>
2025-06-01 03:53:45 +0600

    fixed by reanming image to favicon ramadan image url

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
 	import Reveal from "$lib/components/reveal.svelte";
 </script>
 
-<div class="text-white text-center bg-[url('logo1.jpg')] bg-cover bg-center bg-fixed bg-no-repeat min-h-screen">
+<div class="text-white text-center bg-[url('favicon.jpg')] bg-cover bg-center bg-fixed bg-no-repeat min-h-screen">
 	<Navbar />
 
 	<div class="flex justify-center items-center mt-32"> 
