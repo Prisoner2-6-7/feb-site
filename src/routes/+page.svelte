c696639244b2b13e3c3e32af31d264be003c0df0
ParrotOS <mukto2004@gmail.com>
2025-06-01 03:36:58 +0600

    converted css to tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,14 @@
 <script>
-    import Navbar from "$lib/components/navbar.svelte";
+	import Navbar from "$lib/components/navbar.svelte";
 	import Reveal from "$lib/components/reveal.svelte";
 </script>
 
+<div class="text-white text-center bg-[url('ramadan.jpg')] bg-cover bg-center bg-fixed bg-no-repeat min-h-screen">
+	<Navbar />
 
-<div class="bg-image min-h-screen">
-    <Navbar></Navbar>
-
-	<div class="flex justify-center items-center mt-20"> <!-- need to change mt-20 -->
-    <Reveal/>
-
+	<div class="flex justify-center items-center mt-32"> 
+		<Reveal />
 	</div>
-
 </div>
-<div class="h-lvh"></div>
 
-<style>
-	.bg-image {
-		color: white;
-		text-align: center;
-		background-image:
-			linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.2)),
-			/* This is the overlay */ url(ramadan.jpg);
-		background-size: cover;
-		background-position: center;
-		background-attachment: fixed;
-	}
-</style>
+<div class="h-screen"></div> 
