cfcfd7dc9f566cd898c88480fbc58aad7e534540
ParrotOS <mukto2004@gmail.com>
2025-06-01 03:45:07 +0600

    fixed by reanming image to logo1 ramadan image url

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
 	import Reveal from "$lib/components/reveal.svelte";
 </script>
 
-<div class="text-white text-center bg-[url('_app/immutable/assets/ramadan.jpg')] bg-cover bg-center bg-fixed bg-no-repeat min-h-screen">
+<div class="text-white text-center bg-[url('logo1.jpg')] bg-cover bg-center bg-fixed bg-no-repeat min-h-screen">
 	<Navbar />
 
 	<div class="flex justify-center items-center mt-32"> 
