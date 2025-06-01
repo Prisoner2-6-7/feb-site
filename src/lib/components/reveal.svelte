0cd3750331d246d2b47e51c56bb9db55e36e0c3f
ParrotOS <mukto2004@gmail.com>
2025-06-01 15:34:56 +0600

    2nd page done

--- src/lib/components/reveal.svelte
+++ src/lib/components/reveal.svelte
@@ -2,30 +2,30 @@
     import BoxReveal from "$lib/components/BoxReveal.svelte";
 </script>
 <div
-  class="h-full w-full max-w-[50rem] items-center justify-center overflow-hidden bg-white/5 backdrop-blur-sm rounded-lg">
+  class="h-full w-full max-w-[50rem] items-center justify-center overflow-hidden bg-white/5 backdrop-blur-sm rounded-lg backdrop-contrast-50 backdrop-grayscale-50">
   <BoxReveal boxColor={"#5046e6"} duration={0.5}>
     <p class="text-[3.5rem] font-semibold">
-      Future Entrepreneurs Bangladesh<span class="text-[#5046e6]">.</span>
+      Future <span class="text-[#5046e6] ">Entrepreneurs</span> Bangladesh.
     </p>
   </BoxReveal>
 
-  <BoxReveal boxColor={"#5046e6"} duration={0.5}>
+  <BoxReveal boxColor={"#5046e6"} duration={1}>
     <h2 class="mt-[.5rem] text-[1rem]">
       Community For{" "}
-      <span class="text-[#5046e6]">Design Engineers</span>
+      <span class="text-[#5046e6]">Entrepreneurs</span>
     </h2>
   </BoxReveal>
 
   <BoxReveal boxColor={"#5046e6"} duration={0.5}>
     <div class="mt-[1.5rem]">
       <p>
-        -&gt; 20+ free and open-source animated components
-        <span class="font-semibold text-[#5046e6]"> Svelte</span>,
-        <span class="font-semibold text-[#5046e6]"> Svelte-Inview</span>,
-        <span class="font-semibold text-[#5046e6]"> Tailwind CSS</span>, and
-        <span class="font-semibold text-[#5046e6]"> Framer Motion</span>
+        -&gt; A open community where people can
+        <span class="font-semibold text-[#5046e6]"> Connect</span>,
+        <span class="font-semibold text-[#5046e6]"> Network</span>,
+        <span class="font-semibold text-[#5046e6]"> Question</span>, and
+        <span class="font-semibold text-[#5046e6]"> Get Help</span>
         . <br />
-        -&gt; 100% open-source, and customizable. <br />
+        -&gt; This is the place where you will expand your career. <br />
       </p>
     </div>
   </BoxReveal>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,45 @@
 <script>
-	import Navbar from "$lib/components/navbar.svelte";
-	import Reveal from "$lib/components/reveal.svelte";
+	import Navbar from '$lib/components/navbar.svelte';
+	import Reveal from '$lib/components/reveal.svelte';
+	import AuroraText from '$lib/components/AuroraText.svelte';
+	import GradualSpacing from '$lib/components/GradualSpacing.svelte';
 </script>
 
-<div class="text-white text-center bg-[url('https://ik.imagekit.io/rs8qlp3dz/ramadan.jpg')] bg-cover bg-center bg-fixed bg-no-repeat min-h-screen">
+<div
+	class="min-h-screen bg-[url('https://ik.imagekit.io/rs8qlp3dz/ramadan.jpg')] bg-cover bg-fixed bg-center bg-no-repeat text-center text-white"
+>
 	<Navbar />
 
-	<div class="flex justify-center items-center mt-32"> 
+	<div class="mt-32 flex items-center justify-center">
 		<Reveal />
 	</div>
 </div>
 
-<div class="h-screen"></div> 
+<div class="min-h-screen">
+	<div class="mx-auto max-w-2xl bg-white p-6 md:p-8">
+		<h1 class="text-4xl font-bold tracking-tighter md:text-5xl lg:text-7xl">
+			Future Entrepreneurs Bangladesh <AuroraText>FEB</AuroraText>
+		</h1>
+		<!-- Paragraph Text -->
+		<p class="mb-8 mt-8 text-base leading-relaxed text-gray-700">
+			In ipsum ullamcorper bibendum fringilla nulla dictum nibh nullam. Duis quam in bibendum
+			gravida tristique dis elementum facilisis tristique. Nunc, dictumst eget purus interdum sed
+			lorem ut non. Tellus diam praesent duis morbi et enim magna nam donec. Vitae, blandit ac
+			tincidunt sollicitudin.
+		</p>
+
+		<!-- Image -->
+	</div>
+	<div class="mx-32 flex items-end justify-center">
+		<div class="">
+			<img
+				src="https://ik.imagekit.io/rs8qlp3dz/feb%20discord%20general.png"
+				alt="Interior view of a church altar with a cross"
+				class=" rounded-lg shadow-md"
+			/>
+		</div>
+	</div>
+</div>
+<div class="min-h-screen bg-amber-50 mt-8">
+
+</div>
