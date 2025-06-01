4a04d12c856946152e1ba943862e21f135a9077b
ParrotOS <mukto2004@gmail.com>
2025-06-01 23:05:38 +0600

    added header tag in meta preview

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,10 +3,18 @@
 	let { children } = $props();
 </script>
 
-<meta property="og:title" content="Future Entrepreneurs Bangladesh" />
-<meta property="og:description" content="An community where like minded peoples connect with each other." />
-<meta property="og:image" content="https://ik.imagekit.io/rs8qlp3dz/image.png?updatedAt=1748797350404" />
-<meta property="og:url" content="https://feb-umber.vercel.app/" />
+<head>
+	<meta property="og:title" content="Future Entrepreneurs Bangladesh" />
+	<meta
+		property="og:description"
+		content="An community where like minded peoples connect with each other."
+	/>
+	<meta
+		property="og:image"
+		content="https://ik.imagekit.io/rs8qlp3dz/image.png?updatedAt=1748797350404"
+	/>
+	<meta property="og:url" content="https://feb-umber.vercel.app/" />
+</head>
 
 {@render children()}
 
