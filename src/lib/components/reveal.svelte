7fc25593eb700410ae979884455be764811def05
ParrotOS <mukto2004@gmail.com>
2025-06-01 17:16:59 +0600

    completed 3rd page

--- src/lib/components/reveal.svelte
+++ src/lib/components/reveal.svelte
@@ -2,7 +2,7 @@
     import BoxReveal from "$lib/components/BoxReveal.svelte";
 </script>
 <div
-  class="h-full w-full max-w-[50rem] items-center justify-center overflow-hidden bg-white/5 backdrop-blur-sm rounded-lg backdrop-contrast-50 backdrop-grayscale-50">
+  class="h-full w-full max-w-[50rem] items-center justify-center overflow-hidden bg-white/5 backdrop-blur-sm rounded-3xl backdrop-contrast-50 backdrop-grayscale-50">
   <BoxReveal boxColor={"#5046e6"} duration={0.5}>
     <p class="text-[3.5rem] font-semibold">
       Future <span class="text-[#5046e6] ">Entrepreneurs</span> Bangladesh.
@@ -10,14 +10,14 @@
   </BoxReveal>
 
   <BoxReveal boxColor={"#5046e6"} duration={1}>
-    <h2 class="mt-[.5rem] text-[1rem]">
+    <h2 class="mt-[.5rem] text-[1rem] mx-4">
       Community For{" "}
       <span class="text-[#5046e6]">Entrepreneurs</span>
     </h2>
   </BoxReveal>
 
   <BoxReveal boxColor={"#5046e6"} duration={0.5}>
-    <div class="mt-[1.5rem]">
+    <div class="mt-[1.5rem] mx-4">
       <p>
         -&gt; A open community where people can
         <span class="font-semibold text-[#5046e6]"> Connect</span>,
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,7 @@
 	import AuroraText from '$lib/components/AuroraText.svelte';
 	import GradualSpacing from '$lib/components/GradualSpacing.svelte';
 	import BorderBeam from '$lib/components/BorderBeam.svelte';
+	import SparklesText from '$lib/components/SparklesText.svelte'
 </script>
 
 <div
@@ -16,7 +17,7 @@
 	</div>
 </div>
 
-<div class="min-h-screen">
+<div class="min-h-screen pb-8">
 	<div class="mx-auto max-w-2xl bg-white p-6 md:p-8">
 		<h1 class="text-4xl font-bold tracking-tighter md:text-5xl lg:text-7xl">
 			Future Entrepreneurs Bangladesh <AuroraText>FEB</AuroraText>
@@ -32,9 +33,7 @@
 		<!-- Image -->
 	</div>
 	<div class="mx-32 flex items-end justify-center">
-		<div
-			class="relative rounded-3xl border-4 "
-		>
+		<div class="relative rounded-3xl border-4">
 			<BorderBeam size={150} duration={12} />
 			<img
 				src="https://ik.imagekit.io/rs8qlp3dz/feb%20discord%20general.png"
@@ -45,30 +44,23 @@
 	</div>
 </div>
 
-<div class="mt-8 min-h-screen bg-amber-50">
+<div class="min-h-screen bg-amber-50 pb-8">
 	<div class="mx-32">
+		
 		<div class="max-w-2x mx-auto p-6 md:p-8">
-			<h1 class="text-4xl font-bold tracking-tighter md:text-5xl lg:text-7xl">
-				Future Entrepreneurs Bangladesh <AuroraText>FEB</AuroraText>
-			</h1>
-			<!-- Paragraph Text -->
-			<p class="mb-8 mt-8 text-base leading-relaxed text-gray-700">
-				In ipsum ullamcorper bibendum fringilla nulla dictum nibh nullam. Duis quam in bibendum
-				gravida tristique dis elementum facilisis tristique. Nunc, dictumst eget purus interdum sed
-				lorem ut non. Tellus diam praesent duis morbi et enim magna nam donec. Vitae, blandit ac
-				tincidunt sollicitudin.
+			<SparklesText/>
+		</div>
+
+		<div class="flex items-center">
+			<img
+				src="https://media.discordapp.net/attachments/1244616153716752445/1377970217896120361/Screenshot_2025-05-30-17-21-28-41_a23b203fd3aafc6dcb84e438dda678b6.jpg?ex=683ce085&is=683b8f05&hm=b71ec0c754836feda5f42400dd8dea70a5fe22e1a2966da08aa29f5262364591&=&format=webp&width=512&height=696"
+				alt="Interior view of a church altar with a cross"
+				class="rounded-lg shadow-md w-[560px] h-[700px] object-cover"				
+			/>
+			<p class="text-center leading-relaxed text-gray-700 font-bold">
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione eum asperiores consectetur quam! Tenetur repellendus nostrum molestiae in corporis, tempora debitis perferendis tempore fugiat itaque hic porro consequuntur ea adipisci?
 			</p>
 
-			<!-- Image -->
-		</div>
-		<div class="flex items-end justify-center">
-			<div class="">
-				<img
-					src="https://media.discordapp.net/attachments/1244616153716752445/1377970217896120361/Screenshot_2025-05-30-17-21-28-41_a23b203fd3aafc6dcb84e438dda678b6.jpg?ex=683ce085&is=683b8f05&hm=b71ec0c754836feda5f42400dd8dea70a5fe22e1a2966da08aa29f5262364591&=&format=webp&width=512&height=696"
-					alt="Interior view of a church altar with a cross"
-					class=" rounded-lg shadow-md"
-				/>
-			</div>
 		</div>
 	</div>
 </div>
