42ec878a353c9198f02b528d7196e04e3ec6aced
ParrotOS <mukto2004@gmail.com>
2025-06-02 12:40:05 +0600

    fixed bg navbar for phones

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -1,11 +1,33 @@
-	<nav class="navbar flex items-center justify-between p-10 bg-transparent">
-		<img class="h-24 w-24" src="logo.gif" alt="Logo"  />
-		<ul class="flex gap-20">
-			<li class="font-extrabold"><a href="#">Home</a></li>
-			<li class="font-extrabold"><a href="#">About</a></li>
-			<li class="font-extrabold"><a href="#">Product</a></li>
-			<li class="font-extrabold"><a href="#">Contact</a></li>
-		</ul>
-	</nav>
+<script>
+	let isOpen = false;
+	const toggleMenu = () => (isOpen = !isOpen);
+</script>
 
-    
+<nav class="navbar flex items-center justify-between p-6 bg-transparent relative">
+	<img class="h-20 w-20" src="logo.gif" alt="Logo" />
+
+	<!-- Hamburger Icon -->
+	<button class="md:hidden text-white" on:click={toggleMenu}>
+		<svg class="w-8 h-8" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
+			<path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+		</svg>
+	</button>
+
+	<!-- Nav Links -->
+	<ul class="hidden md:flex gap-10 font-extrabold text-white">
+		<li><a href="#">Home</a></li>
+		<li><a href="#">About</a></li>
+		<li><a href="#">Product</a></li>
+		<li><a href="#">Contact</a></li>
+	</ul>
+
+	<!-- Mobile Menu -->
+	{#if isOpen}
+		<ul 
+				class="absolute top-full left-0 w-full bg-white/20 backdrop-blur-lg flex flex-col items-center gap-6 py-6 font-extrabold md:hidden z-50">			<li><a href="#">Home</a></li>
+			<li><a href="#">About</a></li>
+			<li><a href="#">Product</a></li>
+			<li><a href="#">Contact</a></li>
+		</ul>
+	{/if}
+</nav>
