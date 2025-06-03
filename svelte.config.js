4d31d22da3d04517041ef9408d857f1e7fbd6acf
ParrotOS <mukto2004@gmail.com>
2025-06-03 12:40:18 +0600

    added image size for vercel config

--- svelte.config.js
+++ svelte.config.js
@@ -5,6 +5,7 @@ export default {
 		adapter: adapter({
 			images: {
 				// sizes: [640, 828, 1200, 1920, 3840],
+				sizes: [16, 32, 48, 64, 96, 80, 128, 256, 384, 512, 640, 768, 828, 960, 1024, 1200, 1366, 1440, 1600, 1920],
 				// formats: ['image/avif', 'image/webp'],
 				// minimumCacheTTL: 300,
 				// domains: ['example-app.vercel.app'],
